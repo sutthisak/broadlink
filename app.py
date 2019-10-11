abf824694d5462e38de1eaf4d559715b0b2fbe8a
sutthisak <sliwsakul@hotmail.com>
2019-10-11 21:09:01 +0700

    update

--- app.py
+++ app.py
@@ -1,2 +1,11 @@
 import broadlink
-print('xyz')
+ 
+devices = broadlink.discover(timeout=5)
+ 
+def is_devices_found():
+    return len(devices) > 0 and devices[0].auth()
+ 
+if(is_devices_found()):
+    devices[0].send_data(AIR_CMD['MITSUBISHI']['POWER']['OFF'])
+else:
+    print('Broadlink device is not found.')
