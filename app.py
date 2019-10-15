d23a91100f0a41f1e844e3bf1e3ec2238ddb7963
sutthisak <sliwsakul@hotmail.com>
2019-10-15 11:13:42 +0700

    update code

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
 import broadlink
+from resources.remote import ir
  
 devices = broadlink.discover(timeout=5)
  
 def is_devices_found():
     return len(devices) > 0 and devices[0].auth()
- 
-if(is_devices_found()):
-    devices[0].send_data(AIR_CMD['MITSUBISHI']['POWER']['OFF'])
-else:
-    print('Broadlink device is not found.')
+
+if __name__ == "__main__":
+    air_purifier_remote = ir.AIR_PURIFIER['HATARI']['HT-AP12']
+    if(is_devices_found()):        
+        devices[0].send_data(air_purifier_remote['POWER']['OFF'])
+        #devices[0].send_data(air_purifier_remote['POWER']['ON'])
+        #devices[0].send_data(air_purifier_remote['SPEED']['1'])
+        #devices[0].send_data(air_purifier_remote['SPEED']['2'])
+        #devices[0].send_data(air_purifier_remote['SPEED']['3'])
+        #devices[0].send_data(air_purifier_remote['SPEED']['4'])
+        #devices[0].send_data(air_purifier_remote['IONIZER']['OFF'])
+        #devices[0].send_data(air_purifier_remote['IONIZER']['ON'])
+        #devices[0].send_data(air_purifier_remote['PLASMA']['OFF'])
+        #devices[0].send_data(air_purifier_remote['PLASMA']['ON'])
+    else:        
+        print('Broadlink device is not found.')
